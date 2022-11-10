7e765b703e7e1a3036412d0611ee0b3b57d205e3
ArpanGupta707 <arpangupta707@gmail.com>
2022-11-10 16:25:45 +0600

    twelve commit

--- src/Pages/Home/OurServices/Servicesingle.js
+++ src/Pages/Home/OurServices/Servicesingle.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const Servicesingle = ({ service }) => {
     const { _id, image_url, title, price, details } = service;
     console.log(service)
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image_url} alt="" /></figure>
+
+
+
+            <PhotoProvider>
+                <PhotoView src="/1.jpg">
+                    <img src={image_url} alt="" />
+
+                </PhotoView>
+            </PhotoProvider>
+
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>Price: {price} </p>
