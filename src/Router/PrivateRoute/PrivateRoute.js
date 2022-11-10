a73ca332dee18c0780afd0ecad931a37f5a15083
ArpanGupta707 <arpangupta707@gmail.com>
2022-11-10 19:00:04 +0600

    seventen commit

--- src/Router/PrivateRoute/PrivateRoute.js
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ children }) => {
 
     if (loading) {
 
-        return <button className="btn loading">Loading...</button>
+        return <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
     }
     if (user) {
 
